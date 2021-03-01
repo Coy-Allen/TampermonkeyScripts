af4c9b0bcf05680cf707d7da23575b5f8b620750
Coy-Allen <coy1010allen@gmail.com>
2021-03-01 12:24:16 -0500

    Fix update link

--- PF2 source redirect.user.js
+++ PF2 source redirect.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         PF2 source redirect
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.2
+// @description  replace source links with anyflip books/pages
+// @updateURL    https://github.com/Coy-Allen/TampermonkeyScripts/raw/main/PF2%20source%20redirect.user.js
+// @downloadURL  https://github.com/Coy-Allen/TampermonkeyScripts/raw/main/PF2%20source%20redirect.user.js
+// @author       coy1010allen
 // @match        https://2e.aonprd.com/*
 // @grant        none
 // ==/UserScript==
@@ -18,7 +19,8 @@ let hrefDict = {
     "https://paizo.com/products/btq01y0m":"https://online.anyflip.com/xnqmx/uygw/mobile/index.html",//bestiary
     "https://paizo.com/products/btq01y0k":"https://online.anyflip.com/xnqmx/bhnw/mobile/index.html",//core rulebook
     "https://paizo.com/products/btq022bc":"https://online.anyflip.com/hznwz/cggq/mobile/index.html",//advanced rulebook
-    "https://paizo.com/products/btq0250x":"https://paizo.com/products/btq0250x",//
+    "https://paizo.com/products/btq021wf":"https://online.anyflip.com/bujx/ibib/mobile/index.html",//Lost Omens: Gods & Magic
+    "https://paizo.com/products/btq0250x":"https://paizo.com/products/btq0250x",//Lost Omens: Pathfinder Society Guide
 }
 let allLinks = document.getElementsByTagName("a");
 for(let i=0;i<allLinks.length;i++){
